f2711e51f5e45e3d99d1395f004d5d33f15ae0a2
Adriano <adriytkr@gmail.com>
2025-03-19 11:50:28 -0300

    tests

--- example/tests/Unit/ExampleTest.php
+++ example/tests/Unit/ExampleTest.php
@@ -12,5 +12,20 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+    public function test2(): void
+    {
+        $this->expectOutputString('hello');
+        print('hello');
+    }
+    public function test():void{
+        $name='Bob';
+        $value='Hello, '.$name;
+
+        $expected='Hello, Bob';
+
+        $this->assertSame($expected,$value);
     }
 }
